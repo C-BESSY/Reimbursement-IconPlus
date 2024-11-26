418f73f6c68043ce1ec3a4e71b4b11eb837db0a0
C-BESSY <charistyfirst@gmail.com>
2024-11-26 14:46:19 +0800

    feat: add trigger for notifications

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
+use Filament\Notifications\Livewire\DatabaseNotifications;
 
+DatabaseNotifications::trigger('filament.notifications.database-notifications-trigger');
 class AppServiceProvider extends ServiceProvider
 {
     /**
